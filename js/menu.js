d40e022af5fb07a5d4871470843e358bc5dec75b
Shoganai-Kiri <u1980958@campus.udg.edu>
2024-03-04 17:16:34 +0100

    index

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,6 @@ var exit = $("#exit");
 
 
 play.on('click', function(){window.location.assign("./html/game.html");});
-$("#options").on('click', function(){window.location.assign("./html/options.html");});
-$("#saves").on('click', function(){ console.error("Opció no implementada");});
-$("#exit").on('click', function(){ console.warn("No es pot sortir!");});
+options.on('click', function(){window.location.assign("./html/options.html");});
+saves.on('click', function(){ console.error("Opció no implementada");});
+exit.on('click', function(){ console.warn("No es pot sortir!");});
