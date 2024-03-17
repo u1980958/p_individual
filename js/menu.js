896933e521108411cf7c517cd76ba773e2e1d17b
u1980958 <160512605+u1980958@users.noreply.github.com>
2024-03-17 11:50:14 +0100

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
-var play = $('#play');
-var options = $('#options');
-var saves = $('#saves');
-var exit = $('#exit');
+$(document).ready(function() {
+  var play = $('#play');
+  var options = $('#options');
+  var saves = $('#saves');
+  var exit = $('#exit');
 
 
-play.on('click', function(){window.location.assign("./html/game.html");});
-options.on('click', function(){window.location.assign("./html/options.html");});
-saves.on('click', function(){ console.error("Opció no implementada");});
-exit.on('click', function(){ console.warn("No es pot sortir!");});
+  play.on('click', function(){window.location.assign("./html/game.html");});
+  options.on('click', function(){window.location.assign("./html/options.html");});
+  saves.on('click', function(){ console.error("Opció no implementada");});
+  exit.on('click', function(){ console.warn("No es pot sortir!");});
+}
